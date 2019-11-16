1c7e74697d34c33d9606ef530ed4ecc333e2828a
volcha <volcha2001@yandex.ru>
2019-11-16 12:46:35 +0300

    first commit

---
+++ lab8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fib(int n) {
+  return (n==1 || n==2)?1:fib(n-1)+fib(n-2);
+}
+
+int main () {
+  int n;
+  while (scanf("%d",&n)) {
+    printf("%d\n",fib(n));
+  }
+  return 0;
+}
