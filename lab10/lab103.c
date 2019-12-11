c9f19c018c6f01f6333778c859dcebd55c4474f2
volcha <volcha2001@yandex.ru>
2019-12-12 00:25:52 +0300

    f

---
+++ lab10/lab103.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fib(int n) {
+  return (n==1 || n==2)?1:fib(n-1)+fib(n-2);
+}
+
+int fib(int n) {
+  return (n==1 || n==2)?1:fib(n-1)+fib(n-2);
+}
+
+int main () {
+  int n;
+  while (scanf("%d",&n)!=EOF) {
+    printf("%d\n",fib(n));
+  }
+  return 0;
+}
